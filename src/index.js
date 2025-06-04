149fd43ee13584db016c4a183620782ede31a7ed
s_qiu <s.qiu@foxmail.com>
2025-06-04 11:36:02 +0800

    feat: Output Management

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
-  // Lodash, currently included via a script, is required for this line to work
+  const btn = document.createElement('button');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
   return element;
 }
 
